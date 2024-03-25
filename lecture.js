a62e72b93d38b89707faf00f49a8dce03d8373ff
Shamim <shamimjkrworld@gmail.com>
2024-03-25 16:49:09 +0600

    second commit

--- lecture.js
+++ lecture.js
@@ -6,6 +6,16 @@ var book ={
     name : 'Javascript OOP',
     author:'Shamim',
     publisher: 'Reality',
-    page:250
+    page:250,
+    print:function(){
+        console.log(this.name, this.author)
+
+    }
 }
-console.log(book);
+// console.log(book);
+// book.print();
+
+console.log('Book Name : ' + book.name);
+console.log('Book Author  : ' +book['author'] );
+
+
