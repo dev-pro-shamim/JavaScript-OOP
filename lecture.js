72ad00c24c1b0c3662018b51907b143ae01e16a8
Shamim <shamimjkrworld@gmail.com>
2024-03-25 16:40:12 +0600

    first commit

---
+++ lecture.js
@@ -0,0 +1,11 @@
+// var obj = {};
+// var obj1 = new Object;
+// console.log(obj);
+// console.log(obj1);
+var book ={
+    name : 'Javascript OOP',
+    author:'Shamim',
+    publisher: 'Reality',
+    page:250
+}
+console.log(book);
